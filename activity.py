d58b59b01a77592287e15371eca203e259718446
Agustin Zubiaga <aguz@sugarlabs.org>
2012-03-09 17:17:55 -0200

    Add Rafael at the header of the file

--- activity.py
+++ activity.py
@@ -3,6 +3,7 @@
 
 # activity.py by:
 #    Agustin Zubiaga <aguz@sugarlabs.com>
+#    Rafael Ortiz <dirakx@gmail.com>
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
